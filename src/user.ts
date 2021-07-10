14b46e125abf6ee7417b16cd69b2b45c3184afe3
Alexander Jampolski <aj@Alexanders-MBP.fritz.box>
2021-07-10 19:17:18 +0200

    lesson 1.2

--- src/user.ts
+++ src/user.ts
@@ -1,16 +1,16 @@
 import { renderBlock } from './lib.js'
 
-export function renderUserBlock (favoriteItemsAmount) {
-  const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'
-  const hasFavoriteItems = favoriteItemsAmount ? true : false
+export function renderUserBlock (name: string, avatar: string, favoriteItemsAmount: number) : void {
+  const hasFavoriteItems = favoriteItemsAmount > 0
+  const favoritesCaption = hasFavoriteItems ? favoriteItemsAmount : 'ничего нет'
 
   renderBlock(
     'user-block',
     `
     <div class="header-container">
-      <img class="avatar" src="/img/avatar.png" alt="Wade Warren" />
+      <img class="avatar" src="${avatar}" alt="${name}" title="${name}" />
       <div class="info">
-          <p class="name">Wade Warren</p>
+          <p class="name">${name}</p>
           <p class="fav">
             <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
           </p>

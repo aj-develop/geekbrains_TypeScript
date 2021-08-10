4a1480068258930c08119afb4bbb77a73754f87c
Alexander <72311423+aj-develop@users.noreply.github.com>
2021-08-10 22:28:10 +0200

    Revert "Урок 4. Работа с JavaScript кодом"

--- src/interfaces/searchCallback.ts
+++
@@ -1,5 +0,0 @@
-import { Place } from './place.js'
-
-export interface SearchCallback {
-  (error?: Error, place?: Array<Place>): void
-}
--- src/interfaces/searchFormData.ts
+++
@@ -1,9 +0,0 @@
-export interface SearchFormData {
-  checkin?: string,
-  checkout?: string,
-  city?: string
-  cityCoordinates?: string,
-  price?: number,
-  homy?: boolean,
-  flatRent?: boolean
-}
